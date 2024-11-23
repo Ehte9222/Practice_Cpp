938ac64272a52499ffcadb2f3394c6f3c84f3d84
Ehtesham <ehtesham9222@gmail.com>
2024-11-23 13:25:01 +0500

    Task 4 has been completed!

---
+++ task4.cpp
@@ -0,0 +1,15 @@
+// Write a program in C++ to print the sum of two numbers using variables.
+// Print the sum of two numbers :
+#include<iostream>
+using namespace std;
+int main() {
+int a, b, sum;
+cout << "\n Input the first number : ";
+cin  >> a;
+cout << "n/ Input the second number : ";
+cin  >> b;
+sum = a + b;
+cout << "\n The sum of two numbers is : " << sum;
+return 0;
+
+}
