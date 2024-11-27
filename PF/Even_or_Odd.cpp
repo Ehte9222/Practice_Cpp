ea4d04c704faeafd4f9656c8b5902931c6c43ef5
Ehtesham <ehtesham9222@gmail.com>
2024-11-28 01:47:38 +0500

    Made a program to find Even and Odd numbers and Solved error in one program.

---
+++ PF/Even_or_Odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream> 
+using namespace std; 
+int main(){ 
+    //Program to find even and odd number.
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (n==0){      
+    cout << "The number is even." << endl;
+    }
+    else 
+    {
+        cout << "The number is odd." << endl; 
+    }
+    return 0;
+}
---
+++ PF/error1.cpp
@@ -0,0 +1,26 @@
+// There are a number of syntax errors in the following program. Locate as many as 
+// you can.
+// What's wrong with this program?
+// #include iostream
+//  using namespace std;
+//  int main();
+//  }
+//  int a, b, c\\ Three integers
+//  a = 3
+//  b = 4
+//  c = a + b
+//  Cout < "The value of c is %d" < C;
+//  return 0;
+//  {
+
+#include <iostream>
+using namespace std;
+ int main(){
+    
+ int a, b, c; // Three integers
+ a = 3;
+ b = 4;
+ c = a + b;
+ cout << "The value of c is: " << c;
+ return 0;
+ }
