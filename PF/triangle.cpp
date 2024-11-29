0988908c250361242c24e98eac4a61fc4a1353dd
Ehtesham <ehtesham9222@gmail.com>
2024-11-30 00:26:36 +0500

    Added files temporary for practice

---
+++ PF/triangle.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Triangle made by asterisks" << endl;
+    cout << "\n    * " <<  "\n   ***" << "\n  *****" << "\n *******"  << endl;
+}
